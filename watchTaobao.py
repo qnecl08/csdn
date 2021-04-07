7afeb3a5f08788a94934439e1ee24dfdb0ce94ce
qnecl08 <lansesd161616831@163.com>
2021-04-08 01:17:56 +0800

    2018年3月26日

--- watchTaobao.py
+++ watchTaobao.py
@@ -1,6 +1,6 @@
 import pickle
 import threading
-
+import taobaoDb
 import time
 from selenium import webdriver
 
@@ -29,27 +29,41 @@ class taobaoOrderWatch(threading.Thread):
         for cookie in obj:
             driver.add_cookie(cookie)
         time.sleep(2)
-        driver.get(
-            "https://trade.taobao.com/trade/itemlist/list_sold_items.htm?action=itemlist/SoldQueryAction&event_submit_do_query=1&auctionStatus=PAID&tabCode=waitSend")
+        driver.get("https://trade.taobao.com/trade/itemlist/list_sold_items.htm?action=itemlist/SoldQueryAction&event_submit_do_query=1&auctionStatus=PAID&tabCode=waitSend")
         while 1:
             print("new round")
             time.sleep(3)
             try:
-                driver.get("https://trade.taobao.com/trade/itemlist/list_sold_items.htm?action=itemlist/SoldQueryAction&event_submit_do_query=1&auctionStatus=PAID&tabCode=waitSend")
+                # driver.get("https://trade.taobao.com/trade/itemlist/list_sold_items.htm?action=itemlist/SoldQueryAction&event_submit_do_query=1&auctionStatus=PAID&tabCode=waitSend")
+                driver.get("https://trade.taobao.com/trade/itemlist/list_sold_items.htm")
                 time.sleep(1)
                 orders = driver.find_elements_by_class_name("trade-order-main")
                 for order in orders:
                     spans = order.find_elements_by_tag_name("span")
                     print("orderNo:", spans[2].text)  # 找到订单号
-                    tables = order.find_elements_by_tag_name("table")
-                    tds = tables[1].find_elements_by_tag_name("td")
-                    a = tds[5].find_element_by_tag_name("a")  # 详情按钮
-                    detailUrl = a.get_attribute("href")
-                    driver.get(detailUrl)
-                    dls = driver.find_elements_by_tag_name("dl")
-                    print("买家留言", dls[3].find_element_by_tag_name("dt").text,dls[3].find_element_by_tag_name("dd").text)
-                    self.appWatch.newOrderCount+=1
-                    driver.back()
+                    orderNo=spans[2].text
+                    if taobaoDb.getOrder(orderNo)==None:
+                        order_type=""
+                        remark=""
+                        tables = order.find_elements_by_tag_name("table")
+                        tds = tables[1].find_elements_by_tag_name("td")
+                        spans=tds[0].find_elements_by_tag_name("span")
+                        productName=spans[2].text
+                        if "CSDN" in productName:
+                            order_type="csdn"
+                        a = tds[5].find_element_by_tag_name("a")  # 详情按钮
+                        detailUrl = a.get_attribute("href")
+                        driver.get(detailUrl)
+                        dls = driver.find_elements_by_tag_name("dl")
+                        try:
+                            if "买家留言" in dls[3].find_element_by_tag_name("dt").text:
+                                remark=dls[3].find_element_by_tag_name("dd").text
+                        except Exception as e:
+                            pass
+                        taobaoDb.newOrder(orderNo,order_type,remark)
+                        self.appWatch.newOrderCount+=1
+                        time.sleep(2)
+                        driver.back()
                 cookies = driver.get_cookies()
                 pickle.dump(cookies, open("./taobao" + ".txt", "wb+"))
             except Exception as e:
